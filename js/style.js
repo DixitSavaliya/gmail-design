4d7a73766f83946f73f506597db10c3440b63cad
vivek bharda <vivekkbharda@gmail.com>
2019-02-28 16:05:58 +0530

    commit

---
+++ js/style.js
@@ -0,0 +1,8 @@
+
+textField = mdc.textField.MDCTextField.attachTo(document.querySelector('.fname'));
+textField = mdc.textField.MDCTextField.attachTo(document.querySelector('.lname'));
+textField = mdc.textField.MDCTextField.attachTo(document.querySelector('.uname'));
+textField = mdc.textField.MDCTextField.attachTo(document.querySelector('.password'));
+textField = mdc.textField.MDCTextField.attachTo(document.querySelector('.confirm'));
+textField = mdc.textField.MDCTextField.attachTo(document.querySelector('.pnumber'));
+
